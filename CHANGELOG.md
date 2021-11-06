594b1fc3187302394351aaad374dd8788a1e1a88
Luis Perez <luispe@fb.com>
2021-11-06 09:53:53 -0700

    mv pytorch/recipes -> torchrecipes
    Summary:
    # Commands
    ```
    cd ~/fbsource/fbcode
    mkdir torchrecipes
    hg mv  pytorch/recipes/* torchrecipes/
    find torchrecipes/ -type f -exec sed -i 's/pytorch\.recipes/torchrecipes/g' {} \;
    find torchrecipes/ -type f -exec sed -i 's/pytorch\/recipes/torchrecipes/g' {} \;
    cd ../..
    find $(fbgs pytorch/recipes --exclude pytorch/recipes | awk -F ':' '{print $1}') -type f -exec sed -i 's/pytorch\/recipes/torchrecipes/g' {} \;
    find $(fbgs pytorch.recipes --exclude pytorch/recipes | awk -F ':' '{print $1}') -type f -exec sed -i 's/pytorch\.recipes/torchrecipes/g' {} \;
    cd ~/fbsource/fbcode
    arc lint --apply-patches | pastry
    ```
    
    # Reviewers
    To make this a bit easier to review, see the stack ending in D32162823. We can't land these independently, but each diff corresponds to individual move commands from above.
    
    allow-large-files
    
    Reviewed By: kiukchung
    
    Differential Revision: D32162958
    
    fbshipit-source-id: 4011fe0e0366213bb45a413fa89cb56b75531369

--- CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-# CHANGELOG
-
-Summary of per-version changes.
--- torchrecipes/core/hydra_plugins/task_path/task_path.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from hydra.core.config_search_path import ConfigSearchPath
-from hydra.plugins.search_path_plugin import SearchPathPlugin
-
-
-class TaskPathPlugin(SearchPathPlugin):
-    def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
-        search_path.append(provider="stl_task", path="pkg://torchrecipes.core.conf")
