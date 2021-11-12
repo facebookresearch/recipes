d0d781a2c96bb8219ce41646c236df07ae7abb94
Kai Zhang <kaizh@fb.com>
2021-11-12 15:51:14 -0800

    helper function to load pre-trained weights
    Summary:
    This diff updates the model fine-tuning wrapper to accept path to pre-trained model weights.
    
    This diff also implemented a script to turn Lightning checkpoint to model weights file.
    
    Reviewed By: sallysyw
    
    Differential Revision: D31938301
    
    fbshipit-source-id: d239f68ca101991ba8b05178cc3edfbaa7677b17

---
+++ torchrecipes/vision/core/utils/model_weights.py
@@ -0,0 +1,55 @@
+import logging
+from typing import Dict
+
+import torch
+from iopath.common.file_io import g_pathmgr
+from torch import nn
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+def load_model_weights(
+    module: nn.Module, weights_path: str, strict: bool = True
+) -> None:
+    """
+    Loads model weights from given model weights path in-place.
+
+    Args:
+        module (nn.Module): module to be operated on.
+        weights_path (str): path to model weight file in state dict format.
+        strict (bool): whether to load state dict in strict mode.
+    """
+    with g_pathmgr.open(weights_path, "rb") as f:
+        weights = torch.load(f, map_location="cpu")
+        module.load_state_dict(weights, strict=strict)
+        logger.info(f"Loaded model weights from {weights_path}.")
+
+
+def extract_model_weights_from_checkpoint(
+    checkpoint_path: str, model_name: str = ""
+) -> Dict[str, torch.Tensor]:
+    """
+    Extracts model weights from given Lightning checkpoint.
+
+    Args:
+        checkpoint_path (str): path to the Lightning checkpoint.
+        model_name (str): name of model attribute in the Lightning module.
+            Set to empty if model is the Lightning module itself.
+    """
+    with g_pathmgr.open(checkpoint_path, "rb") as f:
+        ckpt = torch.load(f, map_location="cpu")
+        if "state_dict" not in ckpt:
+            raise ValueError(
+                'The checkpoint doesn\'t have key "state_dict",'
+                " please make sure it's a valid Lightning checkpoint."
+            )
+        state_dict = ckpt["state_dict"]
+        logger.info(f"Loaded state dict from checkpoint {checkpoint_path}.")
+
+    prefix_len = 0 if not model_name else len(model_name) + 1  # e.g. "model."
+    weights: Dict[str, torch.Tensor] = {}
+    for k, v in state_dict.items():
+        if model_name and not k.startswith(model_name):
+            raise ValueError(f"invalid key={k} doesn't start with {model_name}")
+        weights[k[prefix_len:]] = v
+    return weights
--- torchrecipes/vision/image_classification/tests/test_train_app.py
+++ torchrecipes/vision/image_classification/tests/test_train_app.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 
 # pyre-strict
+import os
+from copy import deepcopy
 from typing import List, Optional
 
+import torch
+from torch import nn
 from torchrecipes.core.test_utils.test_base import BaseTrainAppTestCase
 from torchrecipes.utils.test import tempdir
+from torchrecipes.vision.core.ops.fine_tuning_wrapper import FineTuningWrapper
+from torchrecipes.vision.core.utils.model_weights import load_model_weights
 from torchrecipes.vision.image_classification.train_app import (
     ImageClassificationTrainApp,
 )
+from torchvision.models.resnet import resnet18
+from torchvision.ops.misc import FrozenBatchNorm2d
 
 
 class TestTrainApp(BaseTrainAppTestCase):
@@ -33,3 +41,30 @@ class TestTrainApp(BaseTrainAppTestCase):
         train_app = self._get_train_app(tb_save_dir=root_dir)
         # Train the model with the config
         train_app.train()
+
+    @tempdir
+    def test_fine_tuning(self, root_dir: str) -> None:
+        pretrained = resnet18()
+
+        weights_path = os.path.join(root_dir, "weights.pth")
+        torch.save(pretrained.state_dict(), weights_path)
+
+        # prepare model for fine-tuning
+        trunk = resnet18(norm_layer=FrozenBatchNorm2d)
+        load_model_weights(trunk, weights_path)
+        head = nn.Linear(in_features=512, out_features=10)
+        fine_tune_model = FineTuningWrapper(trunk, "flatten", head)
+        origin_trunk = deepcopy(fine_tune_model.trunk)
+
+        # start fine-tuning
+        classification_train_app = self._get_train_app(tb_save_dir=root_dir)
+        # pyre-ignore[16]: ImageClassificationModule has model
+        classification_train_app.module.model = fine_tune_model
+        classification_train_app.train()
+
+        with torch.no_grad():
+            inp = torch.randn(1, 3, 28, 28)
+            origin_out = origin_trunk(inp)
+            # pyre-ignore[16]: ImageClassificationModule has model
+            tuned_out = classification_train_app.module.model.trunk(inp)
+            self.assertTrue(torch.equal(origin_out["flatten"], tuned_out["flatten"]))
