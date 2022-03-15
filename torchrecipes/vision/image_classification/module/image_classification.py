6c582abe707a36340d193b608ec18387b1fa1a19
Binh Tang <binhtang@fb.com>
2022-03-15 11:27:04 -0700

    Refactor metric usage in image classification recipe
    Summary:
    We simplify metric usage within the image classification module before adapting it to TorchRecipes V2. Specifically,
    * We remove artificially constructed terms in metric configs, including `name` and `config`.
    * We remove unnecessary hooks (e.g. `on_train_epoch_start`), which have already been included and applied by default.
    
    Some of these changes are applicable to the [video classification](https://www.internalfb.com/code/fbsource/[f23bf25f21552928565065521a9ac805855662d5]/fbcode/torchrecipes/vision/video_classification/module/video_classification.py), [uru knowledge distillation](https://www.internalfb.com/code/fbsource/[dc24d53be5dd]/fbcode/torchrecipes/fb/projects/uru_knowledge_distillation/module/knowledge_distillation.py), and  [eval_inference](https://www.internalfb.com/code/fbsource/[f23bf25f21552928565065521a9ac805855662d5]/fbcode/torchrecipes/vision/eval_inference/module/eval_inference.py) recipes as well as the [reference script](https://www.internalfb.com/code/fbsource/[f23bf25f21552928565065521a9ac805855662d5]/fbcode/torchrecipes/vision/references/image_classification/conf/resnet18.yaml?lines=29-37). Please note that there is no change in workflow behavior here.
    
    Reviewed By: jdsgomes
    
    Differential Revision: D34811067
    
    fbshipit-source-id: 5c902a41d358da397c25d6ae1408ea183a865bc0

--- torchrecipes/vision/image_classification/module/image_classification.py
+++ torchrecipes/vision/image_classification/module/image_classification.py
@@ -12,19 +12,23 @@ from typing import Union, Any, Callable, Dict, Iterable, List, Mapping, Optional
 import hydra
 import pytorch_lightning as pl
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from hydra.core.config_store import ConfigStore
 
 # @manual "//github/third-party/omry/omegaconf:omegaconf"
 from omegaconf import MISSING
 from pyre_extensions import none_throws
 from torch.nn import Parameter
-from torch.optim import Optimizer, lr_scheduler
+from torch.optim import Optimizer
 from torchrecipes.core.conf import ModuleConf
 from torchrecipes.utils.config_utils import get_class_config_method, config_entry
 from torchvision.ops._utils import split_normalization_params
 
 
 OptimCallable = Callable[[Union[Iterable[Parameter], List[Dict[str, Any]]]], Optimizer]
+LRSchedulerCallable = Callable[[Optimizer], torch.optim.lr_scheduler._LRScheduler]
+Batch = Union[List[torch.Tensor], Mapping[str, torch.Tensor]]
 
 
 class ImageClassificationModule(pl.LightningModule):
@@ -46,37 +50,26 @@ class ImageClassificationModule(pl.LightningModule):
 
     def __init__(
         self,
-        model: torch.nn.Module,
-        loss: torch.nn.Module,
+        model: nn.Module,
+        loss: nn.Module,
         optim_fn: OptimCallable,
-        metrics: Mapping[str, torch.nn.Module],
-        lr_scheduler_fn: Optional[
-            Callable[[Optimizer], lr_scheduler._LRScheduler]
-        ] = None,
+        metrics: Mapping[str, nn.Module],
+        lr_scheduler_fn: Optional[LRSchedulerCallable] = None,
         apply_softmax: bool = False,
         process_weighted_labels: bool = False,
         norm_weight_decay: float = 0.0,
         lr_scheduler_interval: str = "epoch",
     ) -> None:
         super().__init__()
-        self.model: torch.nn.Module = model
-        self.loss: torch.nn.Module = loss
+        self.model: nn.Module = model
+        self.loss: nn.Module = loss
         self.optim_fn: OptimCallable = optim_fn
-        self.metrics: Mapping[str, torch.nn.Module] = metrics
-        self.lr_scheduler_fn: Optional[
-            Callable[[Optimizer], lr_scheduler._LRScheduler]
-        ] = lr_scheduler_fn
+        self.lr_scheduler_fn: Optional[LRSchedulerCallable] = lr_scheduler_fn
+        self.metrics: nn.ModuleDict = nn.ModuleDict(metrics)
         self.apply_softmax: bool = apply_softmax
         self.process_weighted_labels: bool = process_weighted_labels
         self.norm_weight_decay: float = norm_weight_decay
         self.lr_scheduler_interval: str = lr_scheduler_interval
-
-        self.train_metrics: torch.nn.ModuleDict = torch.nn.ModuleDict()
-        self.val_metrics: torch.nn.ModuleDict = torch.nn.ModuleDict()
-        self.test_metrics: torch.nn.ModuleDict = torch.nn.ModuleDict()
-
-        # pyre-fixme[4]: Attribute must be annotated.
-        self.example_input_array = torch.rand(3, 224, 224).unsqueeze(0)
 
     @config_entry
     @staticmethod
@@ -88,7 +81,7 @@ class ImageClassificationModule(pl.LightningModule):
         # pyre-fixme[2]: Parameter annotation cannot be `Any`.
         optim: Any,
         # pyre-fixme[2]: Parameter annotation cannot contain `Any`.
-        metrics: List[Any],
+        metrics: Any,
         # pyre-fixme[2]: Parameter annotation cannot be `Any`.
         lr_scheduler: Optional[Any] = None,
         apply_softmax: bool = False,
@@ -98,41 +91,20 @@ class ImageClassificationModule(pl.LightningModule):
     ) -> "ImageClassificationModule":
         model = hydra.utils.instantiate(model)
         loss = hydra.utils.instantiate(loss)
-        metric_modules = {
-            metric_conf.name: hydra.utils.instantiate(metric_conf.config)
-            for metric_conf in metrics
-        }
+        metrics = hydra.utils.instantiate(metrics)
         optim_fn = hydra.utils.instantiate(optim)
-        lr_scheduler_fn = None
-        if lr_scheduler:
-            lr_scheduler_fn = hydra.utils.instantiate(lr_scheduler)
+        lr_scheduler_fn = hydra.utils.instantiate(lr_scheduler)
         return ImageClassificationModule(
             model=model,
             loss=loss,
             optim_fn=optim_fn,
-            metrics=metric_modules,
+            metrics=metrics,
             lr_scheduler_fn=lr_scheduler_fn,
             apply_softmax=apply_softmax,
             process_weighted_labels=process_weighted_labels,
             norm_weight_decay=norm_weight_decay,
             lr_scheduler_interval=lr_scheduler_interval,
         )
-
-    def setup(self, stage: Optional[str] = None) -> None:
-        if stage == "fit":
-            self.train_metrics.update(self.metrics)
-            self.val_metrics.update(self.metrics)
-        else:
-            self.test_metrics.update(self.metrics)
-
-    def on_train_epoch_start(self) -> None:
-        self.reset_metrics("train")
-
-    def on_validation_epoch_start(self) -> None:
-        self.reset_metrics("val")
-
-    def on_test_epoch_start(self) -> None:
-        self.reset_metrics("test")
 
     def _postprocess_preds(self, preds: torch.Tensor) -> torch.Tensor:
         """
@@ -144,13 +116,9 @@ class ImageClassificationModule(pl.LightningModule):
         """
         # Shape of prediction tensor should be (batch_size, ...)
         # Softmax will sum to 1 along last dimension
-        return (
-            torch.nn.functional.softmax(preds, dim=-1) if self.apply_softmax else preds
-        )
+        return F.softmax(preds, dim=-1) if self.apply_softmax else preds
 
-    def _postprocess_labels(
-        self, labels: torch.Tensor, phase_type: str
-    ) -> torch.Tensor:
+    def _postprocess_labels(self, labels: torch.Tensor, phase: str) -> torch.Tensor:
         """
         This function postprocesses weighted label vector by converting it
         to index label by applying argmax if user requests it.
@@ -161,90 +129,51 @@ class ImageClassificationModule(pl.LightningModule):
         """
         # TODO: Remove `process_weighted_labels` when torchmetrics support weighted labels
         # https://torchmetrics.readthedocs.io/en/stable/references/modules.html#input-types
-        if self.process_weighted_labels and phase_type == "train":
+        if self.process_weighted_labels and phase == "train":
             return torch.argmax(labels, dim=-1)
         return labels
 
     def compute_metrics(
-        self, pred: torch.Tensor, label: torch.Tensor, phase_type: str
+        self, pred: torch.Tensor, label: torch.Tensor, phase: str
     ) -> Dict[str, torch.Tensor]:
-        metrics_dict = getattr(self, f"{phase_type}_metrics")
-        metrics_result = {}
-        for name, metric in metrics_dict.items():
-            metrics_result[f"Metrics/{phase_type}/{name}"] = metric(pred, label)
-        return metrics_result
+        return {
+            f"metrics/{phase}_{name}": metric(pred, label)
+            for name, metric in self.metrics.items()
+        }
 
-    def reset_metrics(self, phase_type: str) -> None:
-        metrics_dict = getattr(self, f"{phase_type}_metrics")
-        for _, metric in metrics_dict.items():
-            metric.reset()
-
-    # pyre-ignore
-    def forward(self, input: torch.Tensor) -> Any:
+    def forward(self, input: torch.Tensor, *args: Any, **kwargs: Any) -> torch.Tensor:
         return self.model(input)
 
-    # pyre-fixme[3]: Return annotation cannot be `Any`.
-    def compute_loss(self, output: torch.Tensor, target: torch.Tensor) -> Any:
-        return self.loss(output, target)
-
-    def _step(
-        self,
-        batch: Union[List[torch.Tensor], Mapping[str, torch.Tensor]],
-        batch_idx: int,
-        phase_type: str,
-    ) -> Dict[str, Any]:
-        # REQUIRED
+    def _step(self, batch: Batch, phase: str) -> Dict[str, Any]:
         if isinstance(batch, dict) and "input" in batch and "target" in batch:
             input, target = batch["input"], batch["target"]
         else:
             input, target = batch[0], batch[1]
         model_output = self.forward(input)
-        loss = self.compute_loss(model_output, target)
-        loss_key = f"Losses/{phase_type}_loss"
+        loss = self.loss(model_output, target)
+        loss_key = f"losses/{phase}_loss"
         self.log(loss_key, loss, on_step=True, on_epoch=True)
 
-        preds = (
-            self._postprocess_preds(model_output)
-            if self.apply_softmax
-            else model_output
-        )
-        labels = self._postprocess_labels(target, phase_type)
-
-        metrics_result = self.compute_metrics(preds, labels, phase_type)
+        preds = self._postprocess_preds(model_output)
+        labels = self._postprocess_labels(target, phase)
+        metrics_result = self.compute_metrics(preds, labels, phase)
         self.log_dict(metrics_result, on_epoch=True)
 
-        if phase_type == "test":
+        if phase == "test":
             return {"loss": loss, "output": model_output}
         else:
             return {"loss": loss}
 
-    def training_step(
-        self,
-        batch: Union[List[torch.Tensor], Mapping[str, torch.Tensor]],
-        batch_idx: int,
-        *args: Any,
-        **kwargs: Any,
-    ) -> Dict[str, Any]:
-        # REQUIRED
-        return self._step(batch, batch_idx, "train")
+    def training_step(self, batch: Batch, *args: Any, **kwargs: Any) -> Dict[str, Any]:
+        return self._step(batch, "train")
 
     def validation_step(
-        self,
-        batch: Union[List[torch.Tensor], Mapping[str, torch.Tensor]],
-        batch_idx: int,
-        *args: Any,
-        **kwargs: Any,
+        self, batch: Batch, *args: Any, **kwargs: Any
     ) -> Dict[str, Any]:
-        return self._step(batch, batch_idx, "val")
+        return self._step(batch, "val")
 
-    def test_step(
-        self,
-        batch: Union[List[torch.Tensor], Mapping[str, torch.Tensor]],
-        batch_idx: int,
-        *args: Any,
-        **kwargs: Any,
-    ) -> Dict[str, Any]:
-        return self._step(batch, batch_idx, "test")
+    def test_step(self, batch: Batch, *args: Any, **kwargs: Any) -> Dict[str, Any]:
+        return self._step(batch, "test")
 
     def get_optimizer_param_groups(self) -> List[Dict[str, Any]]:
         norm_params, other_params = split_normalization_params(self.model)
@@ -255,14 +184,11 @@ class ImageClassificationModule(pl.LightningModule):
             )
         return param_groups
 
-    def configure_optimizers(
-        self,
-    ) -> Union[torch.optim.Optimizer, Dict[str, Any],]:
+    def configure_optimizers(self) -> Union[Optimizer, Dict[str, Any]]:
         param_groups = self.get_optimizer_param_groups()
         optim = self.optim_fn(param_groups)
         if not self.lr_scheduler_fn:
             return optim
-
         return {
             "optimizer": optim,
             "lr_scheduler": {
@@ -282,7 +208,7 @@ class ImageClassificationModuleConf(ModuleConf):
     # pyre-fixme[4]: Attribute annotation cannot be `Any`.
     optim: Any = MISSING
     # pyre-fixme[4]: Attribute annotation cannot be `Any`.
-    metrics: List[Any] = MISSING
+    metrics: Any = MISSING
     # pyre-fixme[4]: Attribute annotation cannot be `Any`.
     lr_scheduler: Optional[Any] = None
     apply_softmax: bool = False
